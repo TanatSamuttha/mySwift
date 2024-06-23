1a3aa21c438393e34d16d9ff35788306a4cc6039
Admin <admin@Admins-MacBook-Pro.local>
2024-06-22 20:26:13 -0700

    Commit2.1

--- mySwift/mySwift/ContentView.swift
+++ mySwift/mySwift/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Hello, world!")
             Text("This Github demo")
+            Text("Update")
         }
         .padding()
     }
