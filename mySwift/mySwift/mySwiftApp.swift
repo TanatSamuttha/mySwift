42a96f6a93b67af7686ededef3446fde5d60628b
Admin <admin@Admins-MacBook-Pro.local>
2024-06-22 20:03:54 -0700

    firstCommit

---
+++ mySwift/mySwift/mySwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  mySwiftApp.swift
+//  mySwift
+//
+//  Created by Admin on 22/6/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct mySwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
